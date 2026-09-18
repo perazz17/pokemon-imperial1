770d51254473ee70d0172dc09e4e74c83bf057b2
perazz17 <ckr7vv9mg5@privaterelay.appleid.com>
2026-09-18 12:34:20 +0200

    Recover modular runtime: src/index.js

---
+++ src/index.js
@@ -0,0 +1,4 @@
+/** Public entry point for the incremental modular runtime. */
+export { Game, D } from './game/game.js';
+export { default as DATA } from './data/imperial-data.js';
+export { OverworldController } from './world/overworld-controller.js';
